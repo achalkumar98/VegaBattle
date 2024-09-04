4cf0a0ab7624e9ef76cc194c2fa4925d8daa0d8b
Harsh Vardhan <devharsh2k4@gmail.com>
2024-09-05 01:23:48 +0530

    connected database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
+const mongoURL = process.env.MONGO_URL;
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
     });
 
-app.listen(port, () => {
-    console.log("app is running on port " + port);    
+    mongoose.connect(mongoURL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    }).then(()=>{
+        app.listen(port,()=> console.log(`Server Port: ${port}` ));
+    
+        // User.insertMany(users);
+        // Post.insertMany(posts);
+    }).catch((err)=>{
+        console.log(err,"did not connected");
     });
